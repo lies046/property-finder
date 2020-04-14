a5c38c0c828dfc0476744be5affe1f2d775f5edf
lies046 <skytree.skysky@gmail.com>
2020-04-14 18:19:24 +0900

    Add propertyModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_090626) do
+ActiveRecord::Schema.define(version: 2020_04_14_091331) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2020_04_14_090626) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "properties", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "account_id"
+    t.string "name"
+    t.string "address"
+    t.integer "price"
+    t.integer "rooms"
+    t.integer "bathrooms"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
 end
