f3716e9e7958a8d2f795d9956fe4a74dd6e9ec90
lies046 <skytree.skysky@gmail.com>
2020-04-15 15:00:17 +0900

    Modified PropertiesViews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_091331) do
+ActiveRecord::Schema.define(version: 2020_04_15_044246) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_091331) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parking_spaces", default: 0
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
