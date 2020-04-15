ead07420ec6e7189f1dd72e7e48d6359d9ad19fb
lies046 <skytree.skysky@gmail.com>
2020-04-15 21:55:30 +0900

    Modified LatestProperties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -4,6 +4,10 @@ module PropertiesHelper
     image_tag img, class:"property-thumb"
   end
 
+  def property_thumbnail_url property
+    property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+  end
+
   def property_photo_url property
     property.photo.present? ? property.photo.url : asset_url("placeholder.png")
   end
