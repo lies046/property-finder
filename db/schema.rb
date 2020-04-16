7ec284749da866e19be046f6d8861275b54789d0
lies046 <skytree.skysky@gmail.com>
2020-04-16 17:04:17 +0900

    Add telephone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_043451) do
+ActiveRecord::Schema.define(version: 2020_04_16_074711) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_043451) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "company"
+    t.string "telephone"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
