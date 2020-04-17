f4cee3a3a3023598fe87c58c3ab093f322bda1c3
lies046 <skytree.skysky@gmail.com>
2020-04-17 13:22:57 +0900

    Modified ProfileViews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :telephone, :url, :image, :image_cache, :company])
+    @show_sidebar = true if account_signed_in?
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :telephone, :url, :image, :image_cache, :cover_image, :cover_image_cache, :company, :details])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -6,6 +6,7 @@ class Account < ApplicationRecord
   has_many :properties
 
   mount_uploader :image, ProfilePictureUploader
+  mount_uploader :cover_image, ProfilePictureUploader
 
   def full_name
     "#{first_name} #{last_name}"
